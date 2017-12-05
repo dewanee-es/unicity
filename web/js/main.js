df89bbfa71a0be3c83665a2c189b78ecae72ad15
dewanee-es <eteran5@gmail.com>
2017-12-05 09:29:21 +0100

    Fixes

---
+++ web/js/main.js
@@ -0,0 +1,33 @@
+// TODO
+
+var fonemas = {};
+
+function hablar(personaje, texto) {
+  var $boca = $(personaje).find('.boca');
+  var anteriorFonema = false;
+  var indiceTexto = 0;
+  var hablarTimer = setInterval(function(){
+    var fonema = false;
+    if(indiceTexto < texto.length && !fonema) {
+        fonema = fonemas[texto.substr(indiceTexto, 2)];
+        if(fonemas) {
+          indiceTexto += 2;
+        } else {
+          fonema = fonemas[texto.charAt(indiceTexto++)];
+        }
+    }
+    if(fonema) {
+        if(anteriorFonema) {
+          $(personaje).find('.fonema-' + anteriorFonema).hide();
+        } else {
+          $boca.hide();
+        }
+        $(personaje).find('.fonema-' + fonema).show();
+        anteriorFonema = fonema;
+    } else {
+      clearInterval(hablarTimer);
+      $(personaje).find('.fonema-' + anteriorFonema).hide();
+      $boca.show();
+    }
+  }, 75);
+}
